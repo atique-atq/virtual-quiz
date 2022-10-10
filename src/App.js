afeaf1249c267261ecd092ca75cf93e8a3ad8f77
atique-atq <atique.atq@gmail.com>
2022-10-10 23:39:42 +0600

    added topics api

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       children: [
         {
           path: '/',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Topics></Topics>
         }
       ]
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import Banner from '../Banner/Banner';
+import Topic from '../Topic/Topic';
 
 const Topics = () => {
+    const topics = useLoaderData().data;
+    console.log(topics);
+
     return (
         <div>
             <Banner></Banner>
+            <div className='topics-container'>
+                {
+                    topics.map(topic => <Topic
+                        key={topic.id}
+                        
+                    ></Topic>)
+                }
+
+            </div>
         </div>
     );
 };
