aed6c20c7795611fc45a5afa9542c1659eb28f40
atique-atq <atique.atq@gmail.com>
2022-10-11 22:43:53 +0600

    added toast

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './Question.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ question, index }) => {
-    console.log('------');
     const options = question.options;
+    const clickAnswer = (option) => {
+        if (option === question.correctAnswer) {
+            //react toast message
+            toast.success('Correct answer! Good job!!!');
+        }
+        
+    }
 
     return (
         <div className='question-cart'>
             <h4 className='question'>Quiz {parseInt(index)+1}: {question.question} </h4>
             <div className='question-options'>
                 {
-                    options.map(option => <div className='option'>
+                    options.map(option => <div>
+                        <div className='option' onClick={() => clickAnswer(option)}>
                         <input type="radio" name="" id="" />
                         <span className='option-text'>{option}</span>
-                        
-                        </div>)
+                        </div>
+                        <ToastContainer/>
+                    </div>
+                    )
                 }
             </div>            
         </div>
