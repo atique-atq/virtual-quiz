5929bd8b06b24d4d8f4cdfc810fd3758a9ae71d5
atique-atq <atique.atq@gmail.com>
2022-10-12 07:08:28 +0600

    button changed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Banner.css';
 import banner from '../../images/banner.jpg'
+import { Link } from 'react-router-dom';
 
 
 const Banner = () => {
@@ -9,7 +10,7 @@ const Banner = () => {
             <div className='banner-text'>
                 <h3>Welcome to Virtual Quiz</h3>
                 <p>This platform continues to be a pioneer in education and is constantly innovating, improving and evolving as a online platform. With all our experience and well-designed systems, we are able to ensure a consistently high standard of education.</p>
-                <button>Show Details</button>
+                <button> <Link to="/">Show Details </Link></button>
             </div>
             <div>
                 <img src={banner} alt="" />
